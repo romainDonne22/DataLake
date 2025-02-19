bec8750ba3f06970d56c99693595597b293f9f11
romainDonne22 <rom.donne@gmail.com>
2025-02-19 10:29:25 +0100

    fct analyse

--- dags/Assemble.py
+++ dags/Assemble.py
@@ -1,7 +1,7 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-from Scripts import download_zip_file, extract_zip_file, delete_zip_file, spark_json_to_parquet, spark_save_to_postgress_scrutins, spark_save_to_postgress_deputesActeur, spark_save_to_postgress_deputesOrgane
+from Scripts import download_zip_file, extract_zip_file, delete_zip_file, spark_json_to_parquet, spark_save_to_postgress_scrutins, spark_save_to_postgress_deputesActeur, spark_save_to_postgress_deputesOrgane, analyse
 
 
 # Recuperer la date du jour
@@ -15,89 +15,89 @@ with DAG(
         'retries': 3,
         'retry_delay': timedelta(minutes=5),
     },
-    description='Un DAG pour télécharger, extraire et charger des données JSON à partir d\'un fichier ZIP',
+    description='Un DAG pour étudier qui vote aux scrutins de l Assemblee Nationale',
     schedule_interval=None,  # Pas de planification régulière, déclenché manuellement
     start_date=datetime(2025, 2, 7),
     catchup=False,
 ) as dag:
 
-    # # Tâches du DAG
-    # download_deputes_json_zip = PythonOperator(
-    #     task_id='download_deputes_json_zip',
-    #     python_callable=download_zip_file,
-    #     op_kwargs={
-    #         'url' : "https://data.assemblee-nationale.fr/static/openData/repository/17/amo/deputes_actifs_mandats_actifs_organes/AMO10_deputes_actifs_mandats_actifs_organes.json.zip", # URL de l'API REST qui fournit le fichier ZIP
-    #         'zip_filename' : "Deputes.json.zip" # Nom du fichier ZIP
-    #     }
-    # )
+    # Tâches du DAG
+    download_deputes_json_zip = PythonOperator(
+        task_id='download_deputes_json_zip',
+        python_callable=download_zip_file,
+        op_kwargs={
+            'url' : "https://data.assemblee-nationale.fr/static/openData/repository/17/amo/deputes_actifs_mandats_actifs_organes/AMO10_deputes_actifs_mandats_actifs_organes.json.zip", # URL de l'API REST qui fournit le fichier ZIP
+            'zip_filename' : "Deputes.json.zip" # Nom du fichier ZIP
+        }
+    )
 
-    # download_scrutin_json_zip = PythonOperator(
-    #     task_id='download_scrutin_json_zip',
-    #     python_callable=download_zip_file,
-    #     op_kwargs={
-    #         'url' : "https://data.assemblee-nationale.fr/static/openData/repository/17/loi/scrutins/Scrutins.json.zip",
-    #         'zip_filename' : "Scrutins.json.zip"
-    #     }
-    # )
+    download_scrutin_json_zip = PythonOperator(
+        task_id='download_scrutin_json_zip',
+        python_callable=download_zip_file,
+        op_kwargs={
+            'url' : "https://data.assemblee-nationale.fr/static/openData/repository/17/loi/scrutins/Scrutins.json.zip",
+            'zip_filename' : "Scrutins.json.zip"
+        }
+    )
 
-    # extract_deputes_json_zip = PythonOperator(
-    #     task_id='extract_deputes_json_zip',
-    #     python_callable=extract_zip_file,
-    #     op_kwargs={
-    #         'zip_filename' : "Deputes.json.zip",  # Nom du fichier ZIP
-    #         'extracted_folder' : f"deputes_data/{today}",  # Le dossier 'deputes_data/{date}'
-    #     }
-    # )
+    extract_deputes_json_zip = PythonOperator(
+        task_id='extract_deputes_json_zip',
+        python_callable=extract_zip_file,
+        op_kwargs={
+            'zip_filename' : "Deputes.json.zip",  # Nom du fichier ZIP
+            'extracted_folder' : f"deputes_data/{today}",  # Le dossier 'deputes_data/{date}'
+        }
+    )
 
-    # extract_scrutin_json_zip = PythonOperator(
-    #     task_id='extract_scrutin_json_zip',
-    #     python_callable=extract_zip_file,
-    #     op_kwargs={
-    #         'zip_filename' : "Scrutins.json.zip",  # Nom du fichier ZIP
-    #         'extracted_folder' : f"scrutins_data/{today}", 
-    #     }
-    # )
+    extract_scrutin_json_zip = PythonOperator(
+        task_id='extract_scrutin_json_zip',
+        python_callable=extract_zip_file,
+        op_kwargs={
+            'zip_filename' : "Scrutins.json.zip",  # Nom du fichier ZIP
+            'extracted_folder' : f"scrutins_data/{today}", 
+        }
+    )
 
-    # delete_deputes_json_zip = PythonOperator(
-    #     task_id='delete_deputes_json_zip',
-    #     python_callable=delete_zip_file,
-    #     op_kwargs={
-    #         'zip_filename' : "Deputes.json.zip"
-    #     }
-    # )
+    delete_deputes_json_zip = PythonOperator(
+        task_id='delete_deputes_json_zip',
+        python_callable=delete_zip_file,
+        op_kwargs={
+            'zip_filename' : "Deputes.json.zip"
+        }
+    )
 
-    # delete_scrutins_json_zip = PythonOperator(
-    #     task_id='delete_scrutins_json_zip',
-    #     python_callable=delete_zip_file,
-    #     op_kwargs={
-    #         'zip_filename' : "Scrutins.json.zip"
-    #     }
-    # )
+    delete_scrutins_json_zip = PythonOperator(
+        task_id='delete_scrutins_json_zip',
+        python_callable=delete_zip_file,
+        op_kwargs={
+            'zip_filename' : "Scrutins.json.zip"
+        }
+    )
 
-    # spark_json_to_parquet = PythonOperator(
-    #     task_id='spark_jsontoparquet',
-    #     python_callable=spark_json_to_parquet,
-    #     op_kwargs={
-    #         'deputes_folder' : f"deputes_data/{today}",
-    #         'scrutins_folder' : f"scrutins_data/{today}"
-    #     }
-    # )
+    spark_json_to_parquet = PythonOperator(
+        task_id='spark_jsontoparquet',
+        python_callable=spark_json_to_parquet,
+        op_kwargs={
+            'deputes_folder' : f"deputes_data/{today}",
+            'scrutins_folder' : f"scrutins_data/{today}"
+        }
+    )
 
-    # spark_savetopostgressScrutins = PythonOperator(
-    #     task_id='postgress_scrutins',
-    #     python_callable=spark_save_to_postgress_scrutins,
-    #     op_kwargs={
-    #         'date' : today
-    #     }
-    # )
+    spark_savetopostgressScrutins = PythonOperator(
+        task_id='postgress_scrutins',
+        python_callable=spark_save_to_postgress_scrutins,
+        op_kwargs={
+            'date' : today
+        }
+    )
 
-    # spark_savetopostgressDeputesActeurs = PythonOperator(
-    #     task_id='postgress_deputesActeurs',
-    #     python_callable=spark_save_to_postgress_deputesActeur,
-    #     op_kwargs={
-    #         'date' : today
-    #     }
-    # )
+    spark_savetopostgressDeputesActeurs = PythonOperator(
+        task_id='postgress_deputesActeurs',
+        python_callable=spark_save_to_postgress_deputesActeur,
+        op_kwargs={
+            'date' : today
+        }
+    )
 
     spark_save_to_postgress_deputesOrgane = PythonOperator(
         task_id='postgress_deputesOrgane',
@@ -107,14 +107,12 @@ with DAG(
         }
     )
 
+    analyse = PythonOperator(
+        task_id='analyse',
+        python_callable=analyse
+    )
 
 
     # Ordre d'exécution des tâches
-    # download_deputes_json_zip \
-    # >> extract_deputes_json_zip \
-    # >> delete_deputes_json_zip \
-    # >> download_scrutin_json_zip \
-    # >> extract_scrutin_json_zip \
-    # >> delete_scrutins_json_zip \
-    # >> convert_json_to_parquet
+    [download_deputes_json_zip >> extract_deputes_json_zip >> delete_deputes_json_zip , download_scrutin_json_zip >> extract_scrutin_json_zip >> delete_scrutins_json_zip] >> spark_json_to_parquet >> [spark_savetopostgressScrutins, spark_savetopostgressDeputesActeurs, spark_save_to_postgress_deputesOrgane] >> analyse
     
--- dags/Scripts.py
+++ dags/Scripts.py
@@ -3,6 +3,8 @@ from pyspark.sql import functions as F
 from pyspark.sql.functions import explode, col, from_json, to_date
 from pyspark.sql.types import ArrayType, StructType, StructField, StringType
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+import pandas as pd
+import matplotlib.pyplot as plt
 import requests
 import zipfile
 import os
@@ -263,28 +265,32 @@ def spark_save_to_postgress_deputesActeur(date):
         col("mandat.mandature.datePriseFonction").alias("dateprisefonction")
     )
 
-    # Filtrer les données typeOrgane == "ASSEMBLEE"
-    deputesActeur = deputesActeur.filter((col("typeorgane") == "ASSEMBLEE"))
+    # Filtrer les données
+    deputesActeurfiltre = deputesActeur.filter((col("typeorgane") == "ASSEMBLEE"))
+    parti = deputesActeur.filter((col("typeorgane") == "GP"))
+
+    # Jointure entre deputesActeurfiltre et parti sur la clé commune (par exemple, uid)
+    deputesActeurfiltre = deputesActeurfiltre.join(parti.select("uid", col("organeref").alias("partiid")), on="uid", how="left")
 
     # Mettre au bon format les dates et les colonnes numériques
-    deputesActeur = deputesActeur.withColumn("dateprisefonction", to_date(col("dateprisefonction"), "yyyy-MM-dd"))
-    deputesActeur = deputesActeur.withColumn("placehemicycle", col("placehemicycle").cast("int"))
-    deputesActeur.show()
+    deputesActeurfiltre = deputesActeurfiltre.withColumn("dateprisefonction", to_date(col("dateprisefonction"), "yyyy-MM-dd"))
+    deputesActeurfiltre = deputesActeurfiltre.withColumn("placehemicycle", col("placehemicycle").cast("int"))
+    deputesActeurfiltre.show()
 
     # Collecter les données
-    data = [tuple(row) for row in deputesActeur.collect()]
+    data = [tuple(row) for row in deputesActeurfiltre.collect()]
 
     # Suppression des données précédentes
-    print("Suppression des données précédentes dans la table 'depute'...")
-    cur.execute("DELETE FROM depute")
+    print("Suppression des données précédentes dans la table 'acteur'...")
+    cur.execute("DELETE FROM acteur")
 
-    # Insertion des données dans la table 'depute'
+    # Insertion des données dans la table 'acteur'
     insert_query = """
-        INSERT INTO depute (uid, civ, prenom, nom, typeorgane, organeref, placehemicycle, dateprisefonction)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        INSERT INTO acteur (uid, civ, prenom, nom, typeorgane, organeref, placehemicycle, dateprisefonction, partiid)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
     """
     cur.executemany(insert_query, data)
-    print("Données insérées dans la table 'depute'.")
+    print("Données insérées dans la table 'acteur'.")
 
     conn.commit()
     cur.close()
@@ -338,5 +344,94 @@ def spark_save_to_postgress_deputesOrgane(date):
     conn.commit()
     cur.close()
     conn.close()
+
+# Fonction pour analyser les données et calculer les KPI
+def analyse():
+    # Connexion à la base de données via PostgresHook
+    hook = PostgresHook(postgres_conn_id='postgressRomain')
+    conn = hook.get_conn()
+    cur = conn.cursor()
+
+    # Créer une session Spark
+    spark = SparkSession.builder.appName("LireParquet").getOrCreate()
+
+    # Suppression des données précédentes
+    cur.execute("DELETE FROM cumulscrutin")
+    print("Données supprimées dans la table 'cumulscrutin'.")
+
+    # Requête SQL pour calculer les données cumulées
+    cur.execute("""
+        	INSERT INTO cumulscrutin (datescrutin, nbscrutin, nbscrutincumul, nbscrutincumuldiff)
+			SELECT datescrutin, 
+	                nbscrutin, 
+	                nbscrutincumul,
+	                (MAX(nbscrutincumul) OVER () - nbscrutincumul) + 1 AS nbscrutincumuldiff
+	        FROM (
+	            SELECT datescrutin, 
+	                    nbscrutin,
+	                SUM(nbscrutin) OVER (ORDER BY datescrutin) AS nbscrutincumul
+	            FROM (
+	                SELECT COUNT(*) AS nbscrutin, datescrutin 
+	                FROM (
+	                    SELECT DISTINCT uid, datescrutin 
+	                    FROM scrutin
+	                ) AS sous_requete
+	                GROUP BY datescrutin
+	            ) AS sous_requete_2
+	            ORDER BY datescrutin
+	        ) AS sous_requete_3
+    """
+    )
+    print("Données insérées dans la table 'cumulscrutin'.")
+    
+
+    cur.execute("""
+        SELECT a.uid, 
+            a.nom, 
+            a.prenom, 
+            a.dateprisefonction, 
+            o.libelle, 
+            count(typevote) as nbvote, 
+            sc.nbscrutincumuldiff, 
+            (count(typevote) * 100 / nbscrutincumuldiff) as ratio
+        FROM scrutin s
+        JOIN acteur a ON s.acteurref = a.uid
+        JOIN organe o ON a.partiid = o.uid
+		JOIN cumulscrutin sc ON sc.datescrutin >= a.dateprisefonction
+		WHERE NOT EXISTS ( 
+			SELECT 1
+    		FROM cumulscrutin sc2
+    		WHERE sc2.datescrutin >= a.dateprisefonction
+      		AND sc2.datescrutin < sc.datescrutin
+		)
+        GROUP BY a.uid, a.nom, a.prenom, a.dateprisefonction, o.libelle, sc.nbscrutincumuldiff
+		ORDER BY ratio desc
+    """
+    )
+
+    # Récupérer les résultats de la requête
+    kpi = cur.fetchall()
+    print("KPI calculés avec succès.")
+
+    # Convertir les résultats en DataFrame Pandas
+    columns = [desc[0] for desc in cur.description]
+    kpi_pandas_df = pd.DataFrame(kpi, columns=columns)
+
+    # Tracer la distribution avec Matplotlib
+    plt.figure(figsize=(10, 6))
+    plt.hist(kpi_pandas_df['ratio'], bins=30, edgecolor='k', alpha=0.7)
+    plt.xlabel('Ratio en % (nombre de votes / nombre de scrutins auxquels le député aurait pu participer)')
+    plt.ylabel('Fréquence')
+    plt.title('Distribution des pourcentages de votes par député')
+    plt.grid(True)
+
+    # Enregistrer le graphique dans un fichier image
+    plt.savefig('distribution_kpi_ratios.png')
+    print("Graphique enregistré avec succès.")
+
+    conn.commit()
+    cur.close()
+    conn.close()
+
 
 
